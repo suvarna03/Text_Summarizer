c05f11cf93e396842b569ad79ebcc3d48b6b8941
suvarna03 <suvarna3396@gmail.com>
2025-07-06 18:10:03 +0530

    file creation
    created files with template.py

---
+++ template.py
@@ -0,0 +1,50 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+project_name = "textSummarizer"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+]
+
+def create_file(filepath: Path):
+    filedir = filepath.parent
+    filename = filepath.name
+
+    if filedir:
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file {filename}")
+
+    if not filepath.exists() or filepath.stat().st_size == 0:
+        filepath.touch()
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} already exists")
+
+for file in list_of_files:
+    create_file(Path(file))
